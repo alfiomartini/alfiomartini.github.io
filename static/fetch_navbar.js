582e1f13597e201106bee90bc8755a5f92043c94
Alfio Martini <alfio.martini@gmail.com>
2021-02-19 23:48:48 -0300

    Fixed bug with tooltips (fetch_navbar.js)

--- static/fetch_navbar.js
+++ static/fetch_navbar.js
@@ -1,3 +1,9 @@
+function makeTooltip(){
+  $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+}
+
 fetch("./html_snippets/navbar.html")
 .then(response => {
   return response.text()
@@ -5,8 +11,10 @@ fetch("./html_snippets/navbar.html")
 .then(data => {
   const header =  document.querySelector("header");
   header.innerHTML = data;
+  return data;
   // const navbar = document.querySelector('header .navbar');
   // console.log(navbar);
   // navbar.style.position = 'sticky';
   // navbar.style.top = '20px';
-});
+})
+.then((dummy) => makeTooltip());
